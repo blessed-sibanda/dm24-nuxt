0935f9c8da8175bb7907329c04f3efbbf3cb92e0
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-07-20 17:15:33 +0200

    update the services section

--- components/Services.vue
+++ components/Services.vue
@@ -10,37 +10,63 @@
         <div class="col-md-6">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
             <i class="icofont-angle-45"></i>
-            <h4><a>Structural Steel Design</a></h4>
+            <h4><a>New Structures</a></h4>
+            <p>
+              Design for new commercial, industrial, institutional & residential
+              structures
+            </p>
           </div>
         </div>
         <div class="col-md-6 mt-4 mt-md-0">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
             <i class="icofont-building-alt"></i>
-            <h4><a>Reinforced Concrete Design</a></h4>
+            <h4><a>High Tech Industry Structures</a></h4>
+            <p>
+              Designs for the support of architectural & mechanical components
+              for use in the medical & high tech industry
+            </p>
           </div>
         </div>
         <div class="col-md-6 mt-4 mt-md-0">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
             <i class="icofont-building"></i>
             <h4><a>Multistorey Buildings</a></h4>
+            <p>
+              Design for apartments, hotels, assisted living facilities & other
+              new mixed multi-storey structures using wood, concrete & steel
+            </p>
           </div>
         </div>
         <div class="col-md-6 mt-4 mt-md-0">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
             <i class="icofont-delicious"></i>
-            <h4><a>Special Foundation Design</a></h4>
+            <h4><a>In house designs</a></h4>
+            <p>
+              Design and analysis of steel canopies, stairs & balconies. In
+              house CAD services & on-site plotting of full scale structural
+              drawings
+            </p>
           </div>
         </div>
         <div class="col-md-6 mt-4 mt-md-0">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="500">
             <i class="icofont-home"></i>
-            <h4><a>Houses (From foundation to roof)</a></h4>
+            <h4><a>Storage & Support Structures</a></h4>
+            <p>
+              Design for storage building & canopy structures using light gauge
+              steel framing
+            </p>
           </div>
         </div>
         <div class="col-md-6 mt-4 mt-md-0">
           <div class="icon-box" data-aos="fade-up" data-aos-delay="600">
             <i class="icofont-building-alt"></i>
-            <h4><a>Church Buildings</a></h4>
+            <h4><a>Renovations & Refurbishments</a></h4>
+            <p>
+              Analysis and design for tenant improvements to existing building
+              structures commercial and residential observations existing
+              conditions for real estate insurance and home improvement needs
+            </p>
           </div>
         </div>
       </div>
